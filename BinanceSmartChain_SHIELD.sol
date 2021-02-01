f5c1a5d77c158f84889b65a4070f7b80c3cf6898
Rahul Sharma <66682591+rahulsharma0856@users.noreply.github.com>
2021-02-02 00:10:51 +0530

    Update BinanceSmartChain_SHIELD.sol

--- BinanceSmartChain_SHIELD.sol
+++ BinanceSmartChain_SHIELD.sol
@@ -1,42 +1,33 @@
 /**
  *Submitted for verification at BscScan.com on 2021-01-27
 */
-
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.7.6;
-
 interface IBEP20 {
   /**
    * @dev Returns the amount of tokens in existence.
    */
   function totalSupply() external view returns (uint256);
-
   /**
    * @dev Returns the token decimals.
    */
   function decimals() external view returns (uint256);
-
   /**
    * @dev Returns the token symbol.
    */
   function symbol() external view returns (string memory);
-
   /**
   * @dev Returns the token name.
   */
   function name() external view returns (string memory);
-
   /**
    * @dev Returns the bep token owner.
    */
   function getOwner() external view returns (address);
-
   /**
    * @dev Returns the amount of tokens owned by `account`.
    */
   function balanceOf(address account) external view returns (uint256);
-
   /**
    * @dev Moves `amount` tokens from the caller's account to `recipient`.
    *
@@ -45,7 +36,6 @@ interface IBEP20 {
    * Emits a {Transfer} event.
    */
   function transfer(address recipient, uint256 amount) external returns (bool);
-
   /**
    * @dev Returns the remaining number of tokens that `spender` will be
    * allowed to spend on behalf of `owner` through {transferFrom}. This is
@@ -54,7 +44,6 @@ interface IBEP20 {
    * This value changes when {approve} or {transferFrom} are called.
    */
   function allowance(address _owner, address spender) external view returns (uint256);
-
   /**
    * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
    *
@@ -70,7 +59,6 @@ interface IBEP20 {
    * Emits an {Approval} event.
    */
   function approve(address spender, uint256 amount) external returns (bool);
-
   /**
    * @dev Moves `amount` tokens from `sender` to `recipient` using the
    * allowance mechanism. `amount` is then deducted from the caller's
@@ -81,7 +69,6 @@ interface IBEP20 {
    * Emits a {Transfer} event.
    */
   function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
   /**
    * @dev Emitted when `value` tokens are moved from one account (`from`) to
    * another (`to`).
@@ -89,14 +76,12 @@ interface IBEP20 {
    * Note that `value` may be zero.
    */
   event Transfer(address indexed from, address indexed to, uint256 value);
-
   /**
    * @dev Emitted when the allowance of a `spender` for an `owner` is set by
    * a call to {approve}. `value` is the new allowance.
    */
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
-
 /*
  * @dev Provides information about the current execution context, including the
  * sender of the transaction and its data. While these are generally available
@@ -111,17 +96,14 @@ contract Context {
   // Empty internal constructor, to prevent people from mistakenly deploying
   // an instance of this contract, which should be used via inheritance.
   constructor () { }
-
   function _msgSender() internal view returns (address payable) {
     return msg.sender;
   }
-
   function _msgData() internal view returns (bytes memory) {
     this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
     return msg.data;
   }
 }
-
 /**
  * @dev Wrappers over Solidity's arithmetic operations with added overflow
  * checks.
@@ -148,10 +130,8 @@ library SafeMath {
   function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
     require(c >= a, "SafeMath: addition overflow");
-
     return c;
   }
-
   /**
    * @dev Returns the subtraction of two unsigned integers, reverting on
    * overflow (when the result is negative).
@@ -164,7 +144,6 @@ library SafeMath {
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     return sub(a, b, "SafeMath: subtraction overflow");
   }
-
   /**
    * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
    * overflow (when the result is negative).
@@ -177,10 +156,8 @@ library SafeMath {
   function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
     require(b <= a, errorMessage);
     uint256 c = a - b;
-
     return c;
   }
-
   /**
    * @dev Returns the multiplication of two unsigned integers, reverting on
    * overflow.
@@ -197,13 +174,10 @@ library SafeMath {
     if (a == 0) {
       return 0;
     }
-
     uint256 c = a * b;
     require(c / a == b, "SafeMath: multiplication overflow");
-
     return c;
   }
-
   /**
    * @dev Returns the integer division of two unsigned integers. Reverts on
    * division by zero. The result is rounded towards zero.
@@ -218,7 +192,6 @@ library SafeMath {
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
     return div(a, b, "SafeMath: division by zero");
   }
-
   /**
    * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
    * division by zero. The result is rounded towards zero.
@@ -235,10 +208,8 @@ library SafeMath {
     require(b > 0, errorMessage);
     uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
     return c;
   }
-
   /**
    * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
    * Reverts when dividing by zero.
@@ -253,7 +224,6 @@ library SafeMath {
   function mod(uint256 a, uint256 b) internal pure returns (uint256) {
     return mod(a, b, "SafeMath: modulo by zero");
   }
-
   /**
    * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
    * Reverts with custom message when dividing by zero.
@@ -270,7 +240,6 @@ library SafeMath {
     return a % b;
   }
 }
-
 /**
  * @dev Contract module which provides a basic access control mechanism, where
  * there is an account (an owner) that can be granted exclusive access to
@@ -285,9 +254,7 @@ library SafeMath {
  */
 contract Ownable is Context {
   address private _owner;
-
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
   /**
    * @dev Initializes the contract setting the deployer as the initial owner.
    */
@@ -296,14 +263,12 @@ contract Ownable is Context {
     _owner = msgSender;
     emit OwnershipTransferred(address(0), msgSender);
   }
-
   /**
    * @dev Returns the address of the current owner.
    */
   function owner() public view returns (address) {
     return _owner;
   }
-
   /**
    * @dev Throws if called by any account other than the owner.
    */
@@ -311,7 +276,6 @@ contract Ownable is Context {
     require(_owner == _msgSender(), "Ownable: caller is not the owner");
     _;
   }
-
   /**
    * @dev Leaves the contract without owner. It will not be possible to call
    * `onlyOwner` functions anymore. Can only be called by the current owner.
@@ -323,7 +287,6 @@ contract Ownable is Context {
     emit OwnershipTransferred(_owner, address(0));
     _owner = address(0);
   }
-
   /**
    * @dev Transfers ownership of the contract to a new account (`newOwner`).
    * Can only be called by the current owner.
@@ -331,7 +294,6 @@ contract Ownable is Context {
   function transferOwnership(address newOwner) public onlyOwner {
     _transferOwnership(newOwner);
   }
-
   /**
    * @dev Transfers ownership of the contract to a new account (`newOwner`).
    */
@@ -341,71 +303,58 @@ contract Ownable is Context {
     _owner = newOwner;
   }
 }
-
 contract SHIELDToken is Context, IBEP20, Ownable {
   using SafeMath for uint256;
-
   mapping (address => uint256) private _balances;
-
   mapping (address => mapping (address => uint256)) private _allowances;
-
   uint256 private _totalSupply;
   uint8 public _decimals;
   string public _symbol;
   string public _name;
-
   constructor() {
     _name = "Shield Protocol";
     _symbol = "SHIELD";
     _decimals = 18;
     _totalSupply = 1000000  * (10 ** uint256(_decimals));
     _balances[msg.sender] = _totalSupply;
-
     emit Transfer(address(0), msg.sender, _totalSupply);
   }
-
   /**
    * @dev Returns the bep token owner.
    */
   function getOwner() external view override returns (address) {
     return owner();
   }
-
   /**
    * @dev Returns the token decimals.
    */
   function decimals() external view override returns (uint256) {
     return _decimals;
   }
-
   /**
    * @dev Returns the token symbol.
    */
   function symbol() external view override returns (string memory) {
     return _symbol;
   }
-
   /**
   * @dev Returns the token name.
   */
   function name() external view override returns (string memory) {
     return _name;
   }
-
   /**
    * @dev See {BEP20-totalSupply}.
    */
   function totalSupply() external view override returns (uint256) {
     return _totalSupply;
   }
-
   /**
    * @dev See {BEP20-balanceOf}.
    */
   function balanceOf(address account) external view override returns (uint256) {
     return _balances[account];
   }
-
   /**
    * @dev See {BEP20-transfer}.
    *
@@ -418,14 +367,12 @@ contract SHIELDToken is Context, IBEP20, Ownable {
     _transfer(_msgSender(), recipient, amount);
     return true;
   }
-
   /**
    * @dev See {BEP20-allowance}.
    */
   function allowance(address owner, address spender) external view override returns (uint256) {
     return _allowances[owner][spender];
   }
-
   /**
    * @dev See {BEP20-approve}.
    *
@@ -437,7 +384,6 @@ contract SHIELDToken is Context, IBEP20, Ownable {
     _approve(_msgSender(), spender, amount);
     return true;
   }
-
   /**
    * @dev See {BEP20-transferFrom}.
    *
@@ -455,7 +401,6 @@ contract SHIELDToken is Context, IBEP20, Ownable {
     _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "BEP20: transfer amount exceeds allowance"));
     return true;
   }
-
   /**
    * @dev Atomically increases the allowance granted to `spender` by the caller.
    *
@@ -472,7 +417,6 @@ contract SHIELDToken is Context, IBEP20, Ownable {
     _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
     return true;
   }
-
   /**
    * @dev Atomically decreases the allowance granted to `spender` by the caller.
    *
@@ -491,7 +435,6 @@ contract SHIELDToken is Context, IBEP20, Ownable {
     _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "BEP20: decreased allowance below zero"));
     return true;
   }
-
   /**
    * @dev Moves tokens `amount` from `sender` to `recipient`.
    *
@@ -509,12 +452,10 @@ contract SHIELDToken is Context, IBEP20, Ownable {
   function _transfer(address sender, address recipient, uint256 amount) internal {
     require(sender != address(0), "BEP20: transfer from the zero address");
     require(recipient != address(0), "BEP20: transfer to the zero address");
-
     _balances[sender] = _balances[sender].sub(amount, "BEP20: transfer amount exceeds balance");
     _balances[recipient] = _balances[recipient].add(amount);
     emit Transfer(sender, recipient, amount);
   }
-
   /**
    * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
    *
@@ -531,7 +472,6 @@ contract SHIELDToken is Context, IBEP20, Ownable {
   function _approve(address owner, address spender, uint256 amount) internal {
     require(owner != address(0), "BEP20: approve from the zero address");
     require(spender != address(0), "BEP20: approve to the zero address");
-
     _allowances[owner][spender] = amount;
     emit Approval(owner, spender, amount);
   }
